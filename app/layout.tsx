a0462785aa6b7448036b4f846afbdaf09c03ab86
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2024-01-02 12:56:37 +0900

    add twitter card

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 export const metadata: Metadata = {
   title: "義援金募集",
   description: "石川県能登半島地震への義援金受付状況を開示します",
-  metadataBase: new URL("https://community-xymposium.nemtus.com/"),
   twitter: {
     card: "summary_large_image",
     title: "義援金募集",
